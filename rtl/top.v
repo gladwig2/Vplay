eb433ccf3c04bc28525d97834ca53fa29a45fa88
Ubuntu <ubuntu@ip-172-31-27-118.ec2.internal>
2017-10-02 20:53:31 +0000

    trying to get top to commit

--- rtl/top.v
+++ rtl/top.v
@@ -1,4 +1,4 @@
-// DESCRIPTION: Verilator: Verilog Test module
+// DESCRIPTION: Verilator: Verilog Test module 
 //
 // This file ONLY is placed into the Public Domain, for any use,
 // without warranty, 2003 by Wilson Snyder.
